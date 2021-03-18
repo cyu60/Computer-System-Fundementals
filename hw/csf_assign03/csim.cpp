93f1c3360395391a2cf86284a5b1ecd3e4d9e373
Mehul <magarw13@jhu.edu>
2021-03-18 13:02:41 -0400

    Previous changes

--- hw/csf_assign03/csim.cpp
+++ hw/csf_assign03/csim.cpp
@@ -9,7 +9,6 @@
 #include <tuple>
 #include <sstream> 
 
-
 #include "csim.h"
 
 using std::cin;
@@ -22,7 +21,7 @@ using std::tuple;
 using std::stringstream;
 
 int checkPowerOfTwo(int n);
-std::tuple<uint32_t, bool> readLine(string inputLine, string func1, string func2);
+void readLine(string inputLine, string func1, string func2);
 
 int main(int args, char* argv[]) {
     using std::cin; using std::cout; using std::endl; using std::string; using std::vector; using std::map; using std::tuple;
@@ -45,7 +44,7 @@ int main(int args, char* argv[]) {
     }
     int bytes = atoi(argv[3]);
     if (checkPowerOfTwo(bytes) == 0 || bytes < 4) {
-        fprintf(stderr, "%s\n", "Bytes is not a power of or is less than 4");
+        fprintf(stderr, "%s\n", "Bytes is not a power of 2 or is less than 4");
     }
     string function1 = argv[4];
     string function2 = argv[5];
@@ -55,23 +54,15 @@ int main(int args, char* argv[]) {
     if (function1 == "no-write-allocate" && function2 == "write-back") {
         fprintf(stderr, "%s\n", "No-write-allocate and write-back called together.");
     }
+    vector<string> rawData;
     vector<tuple<uint32_t, bool>> cacheData;
     while (cin) {
         getline(cin, inputLine);
-        cacheData.push_back(readLine(inputLine, function1, function2));
+        rawData.push_back(inputLine);
     }
-    // while (cin) {
-    //     getline(cin, inputLine, '\n');
-    // }
-
-    // Trying to test the input read of the memory? Planning to use string stream?
-    // string test_write;
-    // stringstream input;
-    // while (cin) {
-    //     cin >> input; 
-    //     cout << input.str();
-    // }
-    
+    for (int i = 0; i < rawData.size(); i++) {
+        readLine()
+    }
     return 0; 
 }
 
@@ -89,19 +80,20 @@ int checkPowerOfTwo(int n) {
 //read one line of trace data.
 //returns a tuple that has the memory address and boolean value for the dirty bit
 //also calls other functions to operate on the line. 
-std::tuple<uint32_t, bool> readLine(string inputLine, char* func1, char* func2) {
+
+void readLine(string inputLine, string func1, string func2) { //TODO: change return type to a tuple
     if(inputLine[0] == 's') {
             if (func2.compare("write-back") == 0) {
                 //int result = writeBack()
-            } else if (func2 == "write-through" ) {
+            } else if (func2.compare("write-through") == 0) {
                 //int result = writeThrough(inputLine)
             } else {
                 //fprintf(stderr, "%s\n", "Invalid input for function type");
             }
         } else if (inputLine[0] == 'l') {
-            if(func1 == "write-allocate") {
+            if(func1.compare("write-allocate") == 0) {
                 //int result = writeAllocate()
-            } else if (func1 == "no-write-allocate") {
+            } else if (func1.compare("no-write-allocate") == 0) {
                 //int result = noWriteAllocate()
             } else {
                 //fprintf(stderr, "%s\n", "Invalid input for function");
@@ -111,21 +103,22 @@ std::tuple<uint32_t, bool> readLine(string inputLine, char* func1, char* func2) 
             // exit 1;
         }
 }
-// int writeBack() {
-//     return NULL;
-// }
 
-// int writeThrough() {
-//     return NULL;
-// }
+int writeBack() {
+    return 0;
+}
 
-// int writeAllocate() {
-//     return NULL;
-// }
+int writeThrough() {
+    return 0;
+}
 
-// int noWriteAllocate() {
-//     return NULL;
-// }
+int writeAllocate() {
+    return 0;
+}
+
+int noWriteAllocate() {
+    return 0;
+}
 
 void print_output(int total_loads, int total_stores, int load_hits, int load_misses, int store_hits,int store_misses, int total_cycles) {
     cout << "Total loads: " << total_loads << endl;
